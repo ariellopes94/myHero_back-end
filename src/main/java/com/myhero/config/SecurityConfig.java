45b13da913a9e9082433155bcaf831e11fd9bdd0
Ariel Lopes De Souza <ariel.edit@gmail.com>
2020-10-26 09:24:59 -0400

    Atualizando para gerar cartao qrcode

--- src/main/java/com/myhero/config/SecurityConfig.java
+++ src/main/java/com/myhero/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/pacientes/fichaDoPaciente/{codigoGeradoPeloSistema}",
-			"/pacientes/gerarCodigoQrCode/{cpf}",
+			//"/pacientes/gerarCodigoQrCode/{cpf}",
 			"/alergias",
 			"/doencas",
 			"/medicamentos",
